0327c3a1259bebc2e41c8865f09475a5c65ebed9
LouisVanhove <vanhovelouis123@gmail.com>
2024-12-24 16:57:51 +0100

    Basic movement locally working, EOS key moved

---
+++ Assets/HostMigration/Scripts/MyNetworkManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MyNetworkManager : NetworkManager
+{
+    public override void OnStartServer()
+    {
+        Debug.Log("Server Started!");
+    }
+    public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+    {
+        base.OnServerAddPlayer(conn);
+    }
+    public override void OnStopServer()
+    {
+        Debug.Log("Server Stopped!");
+    }
+
+    public override void OnStartClient() 
+    {
+        Debug.Log("OnStartClient!");
+        // make player activate controls
+
+    }
+    public override void OnStopClient()
+    {
+        Debug.Log("Client stopped!");
+    }
+    public override void OnStopHost()
+    {
+        Debug.Log("Host stopped!");
+    }
+    //Wanna use IEnumerator WaitUntilEndOfFrame()? Should probably do it in start of the actual script instead (call from player for example)
+}
