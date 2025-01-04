bf4e2f2524243971d320e2fbc83363e5f2f4d5a0
LouisVanhove <vanhovelouis123@gmail.com>
2025-01-04 16:39:37 +0100

    Screen debugger looking good

--- Assets/HostMigration/Scripts/Utility/ScreenDebugger.cs
+++ Assets/HostMigration/Scripts/Utility/ScreenDebugger.cs
@@ -1,39 +1,106 @@
 using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
-public class ScreenDebugger : MonoBehaviour
+public class OnScreenDebugger : MonoBehaviour
 {
-    byte _maxQueueItems = 30;  // number of messages to keep
-    Queue _myLogQueue = new();
-
-    void Start()
+    private class LogEntry
     {
-        Debug.Log("Started up on-screen logging.");
+        public string Message;
+        public LogType Type;
+
+        public LogEntry(string message, LogType type)
+        {
+            Message = message;
+            Type = type;
+        }
     }
 
-    void OnEnable()
+    [Header("Log Type Filters")]
+    public bool ShowErrors = true;
+    public bool ShowWarnings = true;
+    public bool ShowDefault = true;
+
+    private List<LogEntry> logEntries = new List<LogEntry>();
+    private Vector2 scrollPosition;
+    private const byte MaxLogs = 50;
+
+    private GUIStyle logStyle;
+
+    private void OnEnable()
     {
         Application.logMessageReceived += HandleLog;
+
     }
 
-    void OnDisable()
+    private void OnDisable()
     {
         Application.logMessageReceived -= HandleLog;
     }
 
-    void HandleLog(string logString, string stackTrace, LogType type)
+    private void HandleLog(string logString, string stackTrace, LogType type)
     {
-        _myLogQueue.Enqueue("[" + type + "] : " + logString);
-        if (type == LogType.Exception)
-            _myLogQueue.Enqueue(stackTrace);
-        while (_myLogQueue.Count > _maxQueueItems)
-            _myLogQueue.Dequeue();
+        logEntries.Add(new LogEntry(logString, type));
+
+        // Limit the number of logs stored
+        if (logEntries.Count > MaxLogs)
+        {
+            logEntries.RemoveAt(0);
+        }
     }
 
-    void OnGUI()
+    private void OnGUI()
     {
-        GUILayout.BeginArea(new Rect(Screen.width - 400, 0, 400, Screen.height));
-        GUILayout.Label("\n" + string.Join("\n", _myLogQueue.ToArray()));
-        GUILayout.EndArea();
+        logStyle = new GUIStyle(GUI.skin.label)
+        {
+            wordWrap = true,
+            fontSize = 11, // Smaller font size
+            padding = new RectOffset(0, 0, -3, -3) // Compact padding
+        };
+        try
+        {
+            GUILayout.BeginArea(new Rect(10, 10, Screen.width - 20, Screen.height - 20));
+            scrollPosition = GUILayout.BeginScrollView(scrollPosition);
+
+            foreach (var logEntry in logEntries)
+            {
+                // Filter logs based on toggle settings
+                if (!ShouldDisplayLog(logEntry.Type))
+                    continue;
+
+                // Set the color based on the log type
+                switch (logEntry.Type)
+                {
+                    case LogType.Log:
+                        logStyle.normal.textColor = Color.white;
+                        break;
+                    case LogType.Warning:
+                        logStyle.normal.textColor = Color.yellow;
+                        break;
+                    case LogType.Error:
+                    case LogType.Exception:
+                        logStyle.normal.textColor = Color.red;
+                        break;
+                    default:
+                        logStyle.normal.textColor = Color.gray;
+                        break;
+                }
+
+                GUILayout.Label(logEntry.Message, logStyle);
+            }
+
+            GUILayout.EndScrollView();
+            GUILayout.EndArea();
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogError($"OnScreenDebugger encountered an error: {ex.Message}");
+        }
+    }
+
+    private bool ShouldDisplayLog(LogType type)
+    {
+        return (ShowErrors && (type == LogType.Error || type == LogType.Exception)) ||
+               (ShowWarnings && type == LogType.Warning) ||
+               (ShowDefault && type == LogType.Log);
     }
 }
